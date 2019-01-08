7b5520a2ab07b107abe1fd665cdce5ceca5d96d6
ReynaDuong <minhduong.md25@gmail.com>
2019-01-08 12:57:45 -0600

    Initial Commit

---
+++ Tappy Hands/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Tappy Hands
+//
+//  Created by Minh Duong on 1/7/19.
+//  Copyright © 2019 Minh Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+}
+
---
+++ Tappy HandsTests/Tappy_HandsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tappy_HandsTests.swift
+//  Tappy HandsTests
+//
+//  Created by Minh Duong on 1/7/19.
+//  Copyright © 2019 Minh Duong. All rights reserved.
+//
+
+import XCTest
+@testable import Tappy_Hands
+
+class Tappy_HandsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
